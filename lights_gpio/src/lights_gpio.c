4c2601807fc974948b57f74c686702fe59a3f3b9
Andrey Chernov <a.o.chernov@mail.ru>
2024-02-14 21:15:09 +0300

    Second homework, optional task

--- lights_gpio/src/lights_gpio.c
+++ lights_gpio/src/lights_gpio.c
@@ -12,6 +12,10 @@
 
 #include <assert.h>
 
+#include <string.h>
+#define NK_USE_UNQUALIFIED_NAMES
+#include <traffic_light/DiagnosticsMessage.idl.h>
+
 /* Type of interface implementing object. */
 typedef struct IModeImpl {
     struct traffic_light_IMode base;     /* Base interface of object */
@@ -54,6 +58,50 @@ static struct traffic_light_IMode *CreateIModeImpl(rtl_uint32_t step)
     impl.step = step;
 
     return &impl.base;
+}
+
+void lightCodeToString(int code, char* string) {
+    switch (code & 0x7) {
+    case 0x1:
+        strcat(string, "red");
+        break;
+    case 0x2:
+        strcat(string, "yellow");
+        break;
+    case 0x4:
+        strcat(string, "green");
+        break;
+    default:
+        strcat(string, "UNKNOWN");
+        break;
+    }
+    if (code & 0x8)
+        strcat(string, " blinking");
+}
+
+void SendMessage(uint32_t id, char* fst, char* snd, struct DiagnosticsMessage_proxy *proxy,
+    DiagnosticsMessage_SendDiagnosticsMessage_req *req, DiagnosticsMessage_SendDiagnosticsMessage_res *res,
+    struct nk_arena *reqArena, struct nk_arena *resArena) {
+
+    nk_arena_reset(reqArena);
+
+    nk_ptr_t *message = nk_arena_alloc(nk_ptr_t, reqArena, &(req->message), 1);
+    if (message == RTL_NULL)
+        return;
+
+    char data[traffic_light_DiagnosticsMessage_SendDiagnosticsMessage_req_message_size] = {0};
+    int length = snprintf(data, traffic_light_DiagnosticsMessage_SendDiagnosticsMessage_req_message_size,
+        "Message #%u: first light -- %s, second light -- %s", id, fst, snd);
+    if (length < 0)
+        return;
+
+    nk_char_t *str = nk_arena_alloc(nk_char_t, reqArena, &message[0], (nk_size_t)(length + 1));
+    if (str == RTL_NULL)
+        return;
+
+    strncpy(str, data, (rtl_size_t)(length + 1));
+
+    DiagnosticsMessage_SendDiagnosticsMessage(&proxy->base, req, reqArena, res, resArena);
 }
 
 /* Lights GPIO entry point. */
@@ -100,8 +148,27 @@ int main(void)
     traffic_light_LightsGPIO_entity entity;
     traffic_light_LightsGPIO_entity_init(&entity, &component);
 
+    /* Communication with 'Diagnostics' */
+    Handle handleDiagnostics = ServiceLocatorConnect("diagnostics_connection");
+    assert(handleDiagnostics != INVALID_HANDLE);
+
+    NkKosTransport transportDiagnostics;
+    NkKosTransport_Init(&transportDiagnostics, handleDiagnostics, NK_NULL, 0);
+
+    nk_iid_t riid = ServiceLocatorGetRiid(handleDiagnostics, "traffic_light.Diagnostics.sendMessage");
+    assert(riid != INVALID_RIID);
+
+    struct DiagnosticsMessage_proxy proxy;
+    DiagnosticsMessage_proxy_init(&proxy, &transportDiagnostics.base, riid);
+
+    DiagnosticsMessage_SendDiagnosticsMessage_req reqDiagnostics;
+    DiagnosticsMessage_SendDiagnosticsMessage_res resDiagnostics;
+    char reqBufferDiagnostics[DiagnosticsMessage_SendDiagnosticsMessage_req_arena_size];
+    struct nk_arena reqArenaDiagnostics = NK_ARENA_INITIALIZER(reqBufferDiagnostics, reqBufferDiagnostics + sizeof(reqBufferDiagnostics));
+
     fprintf(stderr, "Hello I'm LightsGPIO\n");
 
+    uint32_t idx = 0;
     /* Dispatch loop implementation. */
     do
     {
@@ -122,6 +189,15 @@ int main(void)
              */
             traffic_light_LightsGPIO_entity_dispatch(&entity, &req.base_, &req_arena,
                                         &res.base_, &res_arena);
+
+            idx++;
+            char fst[128] = {0};
+            char snd[128] = {0};
+            int result = res.lightsGpio_mode.FMode.res_.result;
+            lightCodeToString(result, fst);
+            lightCodeToString(result >> 8, snd);
+
+            SendMessage(idx, fst, snd, &proxy, &reqDiagnostics, &resDiagnostics, &reqArenaDiagnostics, RTL_NULL);
         }
 
         /* Send response. */
